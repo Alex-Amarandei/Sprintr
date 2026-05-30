2161e01526dd48dd7f8367bd3ece3773f014e35e
Alex Amarandei <alex.m.amarandei@gmail.com>
2026-05-31 00:11:40 +0300

    ✨ Wordmark in dashboard mobile top-bar too

--- web/src/app/dashboard/layout.tsx
+++ web/src/app/dashboard/layout.tsx
@@ -81,7 +81,7 @@ export default async function ShopLayout({
             c="brand.6"
             underline="never"
           >
-            SprintR
+            <Wordmark />
           </LinkAnchor>
           <SignOutButton />
         </Group>
